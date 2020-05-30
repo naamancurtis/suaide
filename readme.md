2e4a40fd479cfddc3dd01cda446d0cbb59197e75
Naaman <naaman.the.dev@gmail.com>
2020-05-30 10:19:31 +0100

    feat(add storage): add json storage of todos

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Todo CLI App
+
+A simple CLI App written purely in Rust to help with the management of Todos & Tasks within the terminal.
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,168 @@
 use chrono::{NaiveDateTime, Utc};
-use clap::{App, Arg};
+use clap::App;
 use dialoguer::{Input, Select};
-use std::io::Error;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::fs::{create_dir, File};
+use std::io::prelude::*;
+use std::io::{BufReader, ErrorKind};
 
-#[derive(Debug)]
-pub struct Todo {
+const DATA_DIR: &str = "./.task-tracker";
+const DATA_PATH: &str = "./.task-tracker/data.json";
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    open: HashMap<Priority, Vec<Task>>,
+    closed: Vec<Task>,
+    next_id: u32,
+}
+
+impl Data {
+    fn new() -> Self {
+        let mut open = HashMap::new();
+        open.insert(Priority::Critical, Vec::new());
+        open.insert(Priority::Today, Vec::new());
+        open.insert(Priority::Week, Vec::new());
+        open.insert(Priority::Sometime, Vec::new());
+        Self {
+            open,
+            closed: Vec::new(),
+            next_id: 1,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
     id: u32,
     message: String,
-    importance: Importance,
+    priority: Priority,
     issue_number: Option<String>,
     opened: NaiveDateTime,
     closed: Option<NaiveDateTime>,
 }
 
-impl Todo {
-    fn new(importance: Importance, message: String, issue_number: Option<String>) -> Self {
+impl Task {
+    fn new(id: u32, priority: Priority, message: String, issue_number: Option<String>) -> Self {
         Self {
-            id: 1,
+            id,
             message,
-            importance,
+            priority,
             issue_number,
             opened: Utc::now().naive_utc(),
             closed: None,
         }
     }
+
+    fn open(&mut self) {
+        self.closed = None;
+    }
+
+    fn complete(&mut self) {
+        self.closed = Some(Utc::now().naive_utc());
+    }
+
+    fn is_complete(&mut self) -> bool {
+        self.closed.is_some()
+    }
+
+    fn get_priority(&self) -> &Priority {
+        &self.priority
+    }
 }
 
-#[derive(Debug)]
-enum Importance {
+#[derive(Debug, Serialize, Deserialize, Eq, Ord, PartialOrd, PartialEq, Hash)]
+enum Priority {
     Critical,
     Today,
     Week,
     Sometime,
 }
 
-impl ToString for Importance {
+impl ToString for Priority {
     fn to_string(&self) -> String {
         match self {
-            Importance::Critical => String::from("Absolultely Critical"),
-            Importance::Sometime => String::from("Sometime in the future"),
-            Importance::Today => String::from("Needs to be done today"),
-            Importance::Week => String::from("Needs to be done sometime this week"),
+            Priority::Critical => String::from("Absolultely Critical"),
+            Priority::Today => String::from("Needs to be done today"),
+            Priority::Week => String::from("Needs to be done sometime this week"),
+            Priority::Sometime => String::from("Sometime in the future"),
         }
     }
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let (mut data, mut file) = get_data()?;
+
     let matches = App::new("Todo-CLI")
-        .version("BETA")
+        .version("β0.1")
         .author("Naaman C. <naaman.the.dev@gmail.com>")
         .about("A simple CLI app to track todos within the terminal")
-        .arg(
-            Arg::new("add")
-                .short('a')
-                .long("add")
-                .value_name("ADD")
-                .takes_value(false)
-                .about("Add a new task or todo"),
-        )
+        .subcommand(App::new("add").about("Add new task"))
+        .subcommand(App::new("list").about("List tasks"))
         .get_matches();
 
     if matches.is_present("add") {
-        println!("Matched Add");
+        data.add_todo()?;
+        data.save_data(&mut file)?;
+    }
 
+    Ok(())
+}
+
+fn get_data() -> Result<(Data, File), Box<dyn Error>> {
+    let file = get_file_handle()?;
+    let reader = BufReader::new(&file);
+
+    match serde_json::from_reader(reader) {
+        Ok(data) => Ok((data, file)),
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+fn get_file_handle() -> Result<File, Box<dyn Error>> {
+    let current_dir = std::env::current_dir()?;
+    let mut permissions = std::fs::metadata(&current_dir)?.permissions();
+    if permissions.readonly() {
+        permissions.set_readonly(false);
+        fs::set_permissions(current_dir, permissions)?;
+    };
+
+    let file = match fs::OpenOptions::new()
+        .write(true)
+        .read(true)
+        .open(DATA_PATH)
+    {
+        Ok(f) => f,
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            create_dir(DATA_DIR)?;
+            let mut new_file = File::create(DATA_PATH)
+                .unwrap_or_else(|e| panic!("Unable to create data file: {:?}", e));
+
+            let data = Data::new();
+            data.save_data(&mut new_file)?;
+            return get_file_handle();
+        }
+
+        Err(e) => return Err(Box::new(e)),
+    };
+    Ok(file)
+}
+
+impl Data {
+    fn save_data(&self, handle: &mut File) -> Result<(), Box<dyn Error>> {
+        handle.seek(std::io::SeekFrom::Start(0));
+        serde_json::to_writer_pretty(handle, self)?;
+        Ok(())
+    }
+
+    fn add_todo(&mut self) -> Result<(), Box<dyn Error>> {
         let mut priority_options = vec![
-            Importance::Today,
-            Importance::Critical,
-            Importance::Week,
-            Importance::Sometime,
+            Priority::Today,
+            Priority::Critical,
+            Priority::Week,
+            Priority::Sometime,
         ];
 
         let priority = Select::new()
@@ -85,16 +180,19 @@ fn main() -> Result<(), Error> {
         {
             Ok(issue_number) if !issue_number.is_empty() => Some(issue_number),
             Ok(_) => None,
-            Err(e) => return Err(e),
+            Err(e) => return Err(Box::new(e)),
         };
 
         let message = Input::<String>::new()
             .with_prompt("What is the task?")
             .interact()?;
 
-        let todo = Todo::new(priority, message, issue_number);
-        println!("{:?}", todo);
-    }
+        let task = Task::new(self.next_id, priority, message, issue_number);
+        self.next_id += 1;
+        if let Some(bucket) = self.open.get_mut(task.get_priority()) {
+            bucket.push(task);
+        }
 
-    Ok(())
+        Ok(())
+    }
 }
